3ff90ba3ec6bc96074777404a12d792f68704b3d
Warwick Stone <uozu.aho@gmail.com>
2019-02-21 23:17:52 +1100

    WIP: binary heap plus tests

---
+++ ailib.test/DataStructures/BinaryHeapTests.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using ailib.DataStructures;
+using NUnit.Framework;
+
+namespace ailib.test.DataStructures
+{
+    public class BinaryHeapTests
+    {
+        private BinaryHeap<int> h;
+
+        private class IntComparer : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                return x < y ? -1 : x > y ? 1 : 0;
+            }
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            h = new BinaryHeap<int>(new IntComparer());
+        }
+
+        [Test]
+        public void Should_throw_on_emtpy_peek()
+        {
+            Assert.That(() => h.PeekMin(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Should_throw_on_emtpy_removes()
+        {
+            Assert.That(() => h.Remove(1), Throws.InvalidOperationException);
+            Assert.That(() => h.RemoveMin(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Should_have_0_size_when_empty()
+        {
+            Assert.AreEqual(0, h.Size);
+        }
+
+        [Test]
+        public void Should_return_false_on_empty_contains()
+        {
+            Assert.IsFalse(h.Contains(32));
+        }
+
+        [Test]
+        public void Should_return_false_on_empty_contains()
+        {
+            Assert.IsFalse(h.Contains(32));
+        }
+
+            it('add and remove single', function() {
+                h.add(55);
+                expect(h.contains(55)).toBe(true);
+                expect(h.peekMin()).toBe(55);
+                expect(h.size()).toBe(1);
+                expect(h.removeMin()).toBe(55);
+                expect(h.size()).toBe(0);
+                expect(h.contains(55)).toBe(false);
+            });
+
+            it('add and remove sequence', function() {
+                h.add(1);
+                h.add(2);
+                h.add(3);
+                expect(h.removeMin()).toBe(1);
+                expect(h.removeMin()).toBe(2);
+                expect(h.removeMin()).toBe(3);
+            });
+
+            it('add and remove reverse sequence', function() {
+                h.add(3);
+                h.add(2);
+                h.add(1);
+                expect(h.removeMin()).toBe(1);
+                expect(h.removeMin()).toBe(2);
+                expect(h.removeMin()).toBe(3);
+            });
+
+            it('should throw on removing non-existent item', function() {
+                h.add(3);
+                expect(() => h.remove(4)).toThrow();
+            });
+
+            it('remove', function() {
+                h.add(3);
+                h.add(2);
+                h.add(1);
+                h.remove(2);
+                expect(h.peekMin()).toBe(1);
+                expect(h.size()).toBe(2);
+            });
+        });
+
+    }
+}
---
+++ ailib/DataStructures/BinaryHeap.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ailib.DataStructures
+{
+    /// BinaryHeap, implemented with array storage
+    public class BinaryHeap<T>
+    {
+        public int Size { get; private set; }
+        private readonly T[] _buf;
+        private readonly IComparer<T> _comparer;
+
+        /**
+         * @param compare Optional comparator. Default is a < b ? -1 : a > b ? 1 : 0.
+         *                Reverse this to make a max heap.
+         */
+        public BinaryHeap(IComparer<T> comparer)
+        {
+            _comparer = comparer;
+        }
+
+        public void Add(T item)
+        {
+            _buf[Size++] = item;
+            Swim(Size - 1);
+        }
+
+        public void Remove(T item)
+        {
+            if (Size == 0) throw new InvalidOperationException("cannot remove from empty");
+
+            var idx = indexOf(item, 0);
+
+            if (idx == -1)
+            {
+                throw new InvalidOperationException("cannot remove item not in heap");
+            }
+
+            RemoveAtIdx(idx);
+        }
+
+        public bool Contains(T item)
+        {
+            return indexOf(item, 0) >= 0;
+        }
+
+        /** Remove the minimum item */
+        public T RemoveMin()
+        {
+            return RemoveAtIdx(0);
+        }
+
+        /** Get the minimum item without removing it */
+        public T PeekMin()
+        {
+            if (Size == 0) throw new InvalidOperationException("cannot peek when empty");
+            return _buf[0];
+        }
+
+        private T RemoveAtIdx(int idx)
+        {
+            if (idx >= Size) throw new ArgumentOutOfRangeException();
+
+            // swap item at idx and last item
+            var temp = _buf[idx];
+            _buf[idx] = _buf[--Size];
+            // sink last item placed at idx
+            Sink(idx);
+            return temp;
+        }
+
+        /** Return the first found index of the given item, else -1
+         *  @param subroot node to start the search.
+         */
+        private int indexOf(T item, int subroot)
+        {
+            if (subroot >= Size) {
+                // gone past leaf
+                return -1;
+            }
+            if (_comparer.Compare(item, this._buf[subroot]) == -1)
+            {
+                // item is less than current node - will not be in this subtree
+                return -1;
+            }
+            if (item.Equals(_buf[subroot]))
+            {
+                return subroot;
+            }
+            else
+            {
+                var idx = indexOf(item, subroot * 2 + 1);
+                return idx >= 0
+                    ? idx
+                    : this.indexOf(item, subroot * 2 + 2);
+            }
+        }
+
+        private void Swim(int idx)
+        {
+            if (idx == 0) return;
+
+            var parentIdx = (idx - 1) / 2;
+
+            while (_comparer.Compare(_buf[idx], _buf[parentIdx]) == -1)
+            {
+                // swap if child < parent
+                Swap(idx, parentIdx);
+                if (parentIdx == 0) return;
+                idx = parentIdx;
+                parentIdx = (idx - 1) / 2;
+            }
+        }
+
+        private void Sink(int idx)
+        {
+            while (true)
+            {
+                var leftIdx = 2 * idx + 1;
+                var rightIdx = 2 * idx + 2;
+                // stop if no children
+                if (leftIdx >= Size) return;
+                // get minimum child
+                var minIdx = leftIdx;
+                if (rightIdx < Size && _comparer.Compare(_buf[rightIdx], _buf[leftIdx]) == -1)
+                {
+                    minIdx = rightIdx;
+                }
+                // swap if parent > min child
+                if (_comparer.Compare(_buf[idx], _buf[minIdx]) == 1)
+                {
+                    Swap(idx, minIdx);
+                }
+                else
+                {
+                    return;
+                }
+                idx = minIdx;
+            }
+        }
+
+        /** swap items at given indexes */
+        private void Swap(int idxA, int idxB)
+        {
+            var temp = _buf[idxA];
+            _buf[idxA] = _buf[idxB];
+            _buf[idxB] = temp;
+        }
+    }
+}
