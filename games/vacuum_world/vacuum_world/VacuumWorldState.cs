4641e2112cd80eefbaf63121928fd7d27db7fb4f
Warwick Stone <uozu.aho@gmail.com>
2019-04-17 21:54:57 +1000

    reorganise dirs

--- games/vacuum_world/vacuum_world/VacuumWorldState.cs
+++ games/vacuum_world/vacuum_world/VacuumWorldState.cs
@@ -166,16 +166,4 @@ namespace vacuum_world
             return sb.ToString();
         }
     }
-
-    public struct Point2D
-    {
-        public int X { get; }
-        public int Y { get; }
-        
-        public Point2D(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-    }
 }
