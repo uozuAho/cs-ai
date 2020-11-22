050e94ff87f8db5e0e5959697dee5a811f3ac1d2
Warwick Stone <uozu.aho@gmail.com>
2020-11-22 21:08:23 +1100

    move e2e tests to game tests

--- games/tic_tac_toe/TicTacToe.Env.Test/TicTacToeGameTests.cs
+++ games/tic_tac_toe/TicTacToe.Env.Test/TicTacToeGameTests.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using NSubstitute;
 using NUnit.Framework;
+using TicTacToe.Game.Test.Utils;
 
 namespace TicTacToe.Game.Test
 {
@@ -67,6 +68,28 @@ namespace TicTacToe.Game.Test
             var actions = _game.GetAvailableActions().ToList();
 
             Assert.AreEqual(8, actions.Count);
+        }
+
+        [Test]
+        public void Player1X_WinsInThreeMoves_Diagonal()
+        {
+            var player1 = new TestPlayer(BoardTile.X);
+            player1.SetMoves(new[] { 0, 4, 8 });
+            var player2 = new TestPlayer(BoardTile.O);
+            player2.SetMoves(new[] { 1, 2 });
+
+            var game = new TicTacToeGame(new Board(), player1, player2);
+
+            // act
+            game.Run();
+
+            // assert
+            Assert.That(game.IsFinished());
+            var expectedBoardState = Board.CreateFromString("xoo" +
+                                                            " x " +
+                                                            "  x");
+            Assert.That(game.Board.IsSameStateAs(expectedBoardState));
+            Assert.AreEqual(BoardTile.X, game.Winner());
         }
     }
 }
