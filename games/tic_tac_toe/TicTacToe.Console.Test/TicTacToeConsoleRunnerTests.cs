57560ab2f3e057d33ab7cf7302ef5b85b0f1611a
Warwick Stone <uozu.aho@gmail.com>
2020-11-22 21:15:48 +1100

    extract TicTacToeConsoleRunner, WIP test

---
+++ games/tic_tac_toe/TicTacToe.Console.Test/TicTacToeConsoleRunnerTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace TicTacToe.Console.Test
+{
+    public class TicTacToeConsoleRunnerTests
+    {
+        private TicTacToeConsoleRunner _runner;
+
+        [SetUp]
+        public void Setup()
+        {
+            _runner = new TicTacToeConsoleRunner();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // _runner.Run();
+        }
+    }
+}
