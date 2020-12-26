e735498e3a3ca7fac0832019760d6be91e1aae4c
Warwick Stone <uozu.aho@gmail.com>
2020-12-26 20:39:34 +1100

    show increasing accuracy of mc

--- environments/random_walk/random_walk.Playground/Program.cs
+++ environments/random_walk/random_walk.Playground/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using random_walk.Playground.mc;
 
@@ -10,11 +11,30 @@ namespace random_walk.Playground
         {
             var env = new RandomWalkEnvironment(5, 3);
 
-            var mcEstimator = new RandomWalkMcValueEstimator();
-            var mcEstimates = mcEstimator.Estimate(env);
+            var actualValues = new[]
+            {
+                1.0 / 6,
+                2.0 / 6,
+                3.0 / 6,
+                4.0 / 6,
+                5.0 / 6
+            };
 
-            var formattedEstimates = mcEstimates.Select(e => $"{e:F}");
-            Console.WriteLine($"mc estimates: {string.Join(",", formattedEstimates)}");
+            var mcEstimator = new RandomWalkMcValueEstimator();
+            var mcEstimates10 = mcEstimator.Estimate(env, 10);
+            var mcEstimates100 = mcEstimator.Estimate(env, 100);
+            var mcEstimates1000 = mcEstimator.Estimate(env, 1000);
+
+            PrintValues(mcEstimates10);
+            PrintValues(mcEstimates100);
+            PrintValues(mcEstimates1000);
+            PrintValues(actualValues);
+        }
+
+        private static void PrintValues(IEnumerable<double> estimates)
+        {
+            var formattedEstimates = estimates.Select(e => $"{e:F}");
+            Console.WriteLine($"estimates: {string.Join(",", formattedEstimates)}");
         }
     }
 }
