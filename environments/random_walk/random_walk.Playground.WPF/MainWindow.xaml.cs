78ff8e9223e0e89bb5ab56702adf3852f69fd7e9
Warwick Stone <uozu.aho@gmail.com>
2020-12-26 20:32:48 +1100

    WIP: view mc error

---
+++ environments/random_walk/random_walk.Playground.WPF/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace random_walk.Playground.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            double[] dataX = new double[] { 1, 2, 3, 4, 5 };
+            double[] dataY = new double[] { 1, 4, 9, 16, 25 };
+            onlyOne.plt.PlotScatter(dataX, dataY);
+            onlyOne.Render();
+        }
+    }
+}
