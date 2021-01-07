3413bf0acfe1cdc41eca63665360778cef1f7d39
Warwick Stone <uozu.aho@gmail.com>
2021-01-07 16:24:19 +1100

    copy td0 agent to nstep agent

---
+++ environments/tic_tac_toe/TicTacToe.Agent.Test/Agents/NStepAgentTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using TicTacToe.Agent.Agents;
+using TicTacToe.Agent.Storage;
+using TicTacToe.Game;
+
+namespace TicTacToe.Agent.Test.Agents
+{
+    internal class NStepAgentTests
+    {
+        private NStepAgent _agent;
+
+        [SetUp]
+        public void Setup()
+        {
+            _agent = new NStepAgent(BoardTile.X);
+        }
+
+        [Test]
+        public void Trains()
+        {
+            _agent.Train(new RandomTicTacToePlayer(BoardTile.O), 50);
+            Assert.DoesNotThrow(() => _agent.GetCurrentValues("name", "description"));
+        }
+
+        [Test]
+        public void AfterTraining_PlaysAGameToCompletion()
+        {
+            var opponent = new FirstAvailableSlotPlayer(BoardTile.O);
+            _agent.Train(opponent, 50);
+            var agentPlayer = new GreedyStateValuePlayer(_agent.GetCurrentStateValues(), BoardTile.X);
+            var game = new TicTacToeGame(Board.CreateEmptyBoard(), agentPlayer, opponent);
+
+            game.Run();
+
+            Assert.IsTrue(game.IsFinished());
+        }
+
+        [Test]
+        public void Saves_And_Loads()
+        {
+            var opponent = new FirstAvailableSlotPlayer(BoardTile.O);
+            _agent.Train(opponent, 1);
+
+            var path = $"{nameof(NStepAgentTests)}.{nameof(Saves_And_Loads)}.agent.json";
+            _agent.SaveTrainedValues("asdf", path);
+
+            var stateValueTable = PolicyFileIo.LoadStateValueTable(path);
+            Assert.NotNull(stateValueTable);
+        }
+
+        [Test]
+        public void AllStateValues_AreBetween_0_and_1()
+        {
+            var opponent = new FirstAvailableSlotPlayer(BoardTile.O);
+            _agent.Train(opponent, 50);
+
+            var stateValues = _agent.GetCurrentStateValues().All().ToList();
+
+            Assert.IsTrue(stateValues.All(sv => Math.Abs(sv.Item2) >= 0.0 && Math.Abs(sv.Item2) <= 1.0));
+        }
+    }
+}
---
+++ environments/tic_tac_toe/TicTacToe.Agent/Agents/NStepAgent.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using ailib.Utils;
+using MoreLinq;
+using TicTacToe.Agent.Environment;
+using TicTacToe.Agent.Storage;
+using TicTacToe.Agent.Utils;
+using TicTacToe.Game;
+
+namespace TicTacToe.Agent.Agents
+{
+    public class NStepAgent
+    {
+        public BoardTile Tile { get; }
+
+        // e-greedy constant: probability of choosing a random action instead
+        // of the greedy action
+        // possible improvement: reduce over time during training
+        private const double ChanceOfRandomAction = 0.05;
+        private const double LearningRate = 0.05;
+        private readonly Random _random = new();
+
+        private StateValueTable _values;
+
+        public NStepAgent(BoardTile tile)
+        {
+            Tile = tile;
+            _values = new StateValueTable(Tile);
+        }
+
+        public TicTacToeAction GetAction(TicTacToeEnvironment environment)
+        {
+            return ShouldDoExploratoryAction()
+                ? RandomAction(environment)
+                : BestAction(environment.CurrentState);
+        }
+
+        public StateValueTable GetCurrentStateValues()
+        {
+            return _values;
+        }
+
+        public ITicTacToePolicy GetCurrentValues(string name, string description)
+        {
+            var policy = new SerializableStateValueTable(name, description, Tile);
+            policy.SetStateValues(_values);
+            return policy;
+        }
+
+        public void SaveTrainedValues(string agentName, string path)
+        {
+            PolicyFileIo.Save(_values, agentName, "", path);
+        }
+
+        public void Train(ITicTacToePlayer opponent, int? numGamesLimit = null)
+        {
+            var gameCount = 0;
+            var maxGames = numGamesLimit ?? 10000;
+            var env = new TicTacToeEnvironment(opponent);
+            _values = new StateValueTable(Tile);
+
+            var stopwatch = Stopwatch.StartNew();
+
+            for (; gameCount < maxGames; gameCount++)
+            {
+                env.Reset();
+                Board? previousAfterstate = null;
+
+                while (!env.CurrentState.IsGameOver)
+                {
+                    var isExploratoryAction = ShouldDoExploratoryAction();
+                    var action = isExploratoryAction ? RandomAction(env) : BestAction(env.CurrentState);
+                    var afterstate = env.CurrentState.DoAction(action);
+                    env.Step(action);
+
+                    if (previousAfterstate != null && !isExploratoryAction)
+                    {
+                        var tdError = _values.Value(afterstate) - _values.Value(previousAfterstate);
+                        var updatedValue = _values.Value(previousAfterstate) + LearningRate * tdError;
+
+                        _values.SetValue(previousAfterstate, updatedValue);
+                    }
+                    previousAfterstate = afterstate;
+                }
+            }
+
+            Console.WriteLine($"Played {gameCount} games in {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        private TicTacToeAction BestAction(Board board)
+        {
+            return board
+                .AvailableActions()
+                .Select(a => new
+                {
+                    action = a,
+                    board = board.DoAction(a)
+                })
+                .MaxBy(b => _values.Value(b.board))
+                .First().action;
+        }
+
+        private TicTacToeAction RandomAction(TicTacToeEnvironment env)
+        {
+            return _random.Choice(env.ActionSpace());
+        }
+
+        private bool ShouldDoExploratoryAction()
+        {
+            return _random.NextDouble() < ChanceOfRandomAction;
+        }
+    }
+}
