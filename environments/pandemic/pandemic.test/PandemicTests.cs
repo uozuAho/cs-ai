ded5f0244219fc1956906363b98c5f6837f42e0a
Warwick Stone <warwick_stone@fastmail.com>
2021-09-17 22:46:49 +1000

    WIP: moving tests around

--- environments/pandemic/pandemic.test/PandemicTests.cs
+++ environments/pandemic/pandemic.test/PandemicTests.cs
@@ -5,14 +5,13 @@ using pandemic.States;
 
 namespace pandemic.test
 {
-    public class PandemicGameStateTests
+    public class PandemicTests
     {
         [Test]
         public void GivenRealBoard_StateShouldBeReadyToInit()
         {
             var state = new PandemicGameState(PandemicBoard.CreateRealGameBoard());
             
-            // no cubes on any cities
             Assert.AreEqual(0, state.CityStates.Sum(c => c.NumCubes(Colour.Red)));
             Assert.AreEqual(0, state.CityStates.Sum(c => c.NumCubes(Colour.Blue)));
             Assert.AreEqual(0, state.CityStates.Sum(c => c.NumCubes(Colour.Black)));
@@ -25,6 +24,31 @@ namespace pandemic.test
             Assert.AreEqual(24, state.CubePile.NumCubes(Colour.Blue));
             Assert.AreEqual(24, state.CubePile.NumCubes(Colour.Black));
             Assert.AreEqual(24, state.CubePile.NumCubes(Colour.Yellow));
+        }
+
+        [Test]
+        public void asdf()
+        {
+            var state = PandemicGame.Init(PandemicBoard.CreateRealGameBoard());
+
+            Assert.AreEqual(2, state.InfectionRate);
+            Assert.AreEqual(0, state.OutbreakCounter);
+
+            // should have drawn 9 infection cards
+            Assert.AreEqual(48 - 9, state.InfectionDeck.Count);
+            Assert.AreEqual(9, state.InfectionDiscardPile.Count);
+
+            // 9 cities should have cubes on them
+            Assert.AreEqual(3, state.CityStates.Count(c => c.NumCubes(c.Colour) == 3));
+            Assert.AreEqual(3, state.CityStates.Count(c => c.NumCubes(c.Colour) == 2));
+            Assert.AreEqual(3, state.CityStates.Count(c => c.NumCubes(c.Colour) == 1));
+
+            // cubes should have been removed from pile
+            var sumOfAllCubesInPile = ColourExtensions.AllColours()
+                .Select(colour => state.CubePile.NumCubes(colour))
+                .Sum();
+
+            Assert.AreEqual(4 * 24 - 9 - 6 - 3, sumOfAllCubesInPile);
         }
     }
 }
--- environments/pandemic/pandemic/PandemicGame.cs
+++ environments/pandemic/pandemic/PandemicGame.cs
@@ -23,5 +23,31 @@ namespace pandemic
         public void DoMove(PlayerMove move)
         {
         }
+
+        public static PandemicGameState Init(PandemicBoard board)
+        {
+            var state = new PandemicGameState(board);
+            InitialInfectCities(state);
+            return state;
+        }
+
+        private static void InitialInfectCities(PandemicGameState state)
+        {
+            for (var numCubes = 1; numCubes <= 3; numCubes++)
+            {
+                for (var i = 0; i < 3; i++)
+                {
+                    var cityName = state.InfectionDeck.Pop();
+                    var city = state.GetCity(cityName);
+                    for (var j = 0; j < numCubes; j++)
+                    {
+                        state.CubePile.RemoveCube(city.Colour);
+                        city.AddCube(city.Colour);
+                    }
+
+                    state.InfectionDiscardPile.Push(cityName);
+                }
+            }
+        }
     }
 }
